b284491bd22dd37e89667b7fe9602ef29a148224
Lenny <valentin805@gmail.com>
2018-06-28 09:19:57 +0300

    Creating an implementation for Password Servicies

---
+++ src/main/java/blog/services/password/PasswordServiciesImpl.java
@@ -0,0 +1,10 @@
+package blog.services.password;
+
+public class PasswordServiciesImpl implements PasswordServicies{
+
+
+    @Override
+    public String generatePassword() {
+        return null;
+    }
+}
